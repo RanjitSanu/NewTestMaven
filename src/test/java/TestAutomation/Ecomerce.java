09e048d3521c91bd96e706986e9d782369396513
RanjitSanu <rpadhiari904@gmail.com>
2023-09-08 00:22:29 +0530

    sample code

---
+++ src/test/java/TestAutomation/Ecomerce.java
@@ -0,0 +1,15 @@
+package TestAutomation;
+
+public class Ecomerce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String s="Ranjit";
+		
+		System.out.println(s);
+		
+	}
+
+}
